77131cd081cd3945a42048c8f73fb903afe05b6d
xhanyifeng <xhanyifeng@gmail.com>
2018-08-26 20:30:55 +0800

    fix top level text bold again

--- src/io/aistarsea/PayloadLego.groovy
+++ src/io/aistarsea/PayloadLego.groovy
@@ -5,7 +5,7 @@ import groovy.json.JsonOutput
 // payload covert to json
 def payloadToJson(channel, text, color, attachments) {
     JsonOutput.toJson([
-        text: **text**,
+        text: "**${text}**",
         channel: channel,
         user: 'aliasmee',
         attachments: attachments
