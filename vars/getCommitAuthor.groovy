2ca4f8e9ef83e851f21231fff243855b0158aed9
xhanyifeng <xhanyifeng@gmail.com>
2018-08-26 21:46:33 +0800

    fix ref old vari

--- vars/getCommitAuthor.groovy
+++ vars/getCommitAuthor.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 
 def call() {
-    def commit = getGitCommit()
+    def commit = getCommitHash()
     author = sh(returnStdout: true, script: "git --no-pager show -s --format='%an' ${commit}").trim()
     return author
 }
