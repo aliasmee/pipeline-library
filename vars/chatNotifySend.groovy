0956dc945447ca5af75b4327b48200662c6c89cd
xhanyifeng <xhanyifeng@gmail.com>
2018-08-27 22:23:13 +0800

    test chatnotify

--- vars/chatNotifySend.groovy
+++ vars/chatNotifySend.groovy
@@ -14,7 +14,10 @@ def call(Map config = [:]) {
     // }
 //    color = config.color?: '#00BFFF'
     def mmUtils = new PayloadLego()
-    def payload = mmUtils.payloadToJson(config.channel, config.attachments, config.text="${JOB_NAME} [${BUILD_DISPLAY_NAME}](${BUILD_URL})")
+    if (!config.text) {
+        config.text = "${JOB_NAME} [${BUILD_DISPLAY_NAME}](${BUILD_URL})"
+    }
+    def payload = mmUtils.payloadToJson(config.channel, config.attachments, config.text)
 
     sh "curl -X POST -d \'payload=${payload}\' ${config.endpoint}"
 }
